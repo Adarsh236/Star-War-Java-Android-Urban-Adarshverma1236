388712fd967dda91d59fb8e31d0d523aa2dda85a
Adarsh Verma <adarshverma11236@gmail.com>
2019-12-08 01:46:48 +0200

    Finishing work with res files and running successfully rs p 1

---
+++ StarWarJavaAndroidUrbanAdarshverma1236/app/src/main/java/com/example/star_war_java_android_urban_adarshverma1236/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.star_war_java_android_urban_adarshverma1236;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
